57c8f140fb4757390de251c5f3096b4175c80ca4
uditmahato <uditmahato29271@gmail.com>
2022-07-14 23:56:40 +0545

    added h

--- osarau/booking_app/admin.py
+++ osarau/booking_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from booking_app.models import Booking
+
+admin.site.register(Booking)
--- osarau/booking_app/models.py
+++ osarau/booking_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    people = models.IntegerField()
+    message = models.TextField()
