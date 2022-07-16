c4b0b97732291a181c885b1596c47b42226e3161
uditmahato <uditmahato29271@gmail.com>
2022-07-16 19:02:49 +0545

    added code in about contact

--- osarau/about_app/models.py
+++ osarau/about_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class about(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
