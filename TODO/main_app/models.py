7a07b9933c491982511914a0114184bbe9ad4649
uditmahato <uditmahato29271@gmail.com>
2022-07-19 18:37:40 +0545

    completed add and see two featuresin todo app

--- TODO/main_app/models.py
+++ TODO/main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    date_created=models.DateTimeField(auto_now_add=True)
+    completion=models.BooleanField(default=False)
+
+    class Meta:
+        db_table="TODO"
+        verbose_name_plural='ToDo app'
+    def __str__(self):
+        return self.title
--- TODO/main_app/views.py
+++ TODO/main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from main_app.models import Todo
+
 # Create your views here.
+def home(request):
+    Todo_list=Todo.objects.all()
+    return render(request,'index.html',{"todos":Todo_list})
+
+def add(request):
+    if request.method=="GET":
+        return render(request,'add.html')
+    else:
+        title=request.POST.get('title')
+        description=request.POST.get('description')
+        Todo.objects.create(title=title,description=description)
+        return home(request)
+
+
